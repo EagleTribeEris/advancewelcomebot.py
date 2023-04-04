d09af901ffc9d948b701fc75d90689f5e3822713
Eagle Tribe Eris <107009952+ErisTheEagleArt@users.noreply.github.com>
2023-04-04 01:01:40 -0700

    DISCORD.PY 2.0 REWRITE
    Did a discord.py 2.0 rewrite of the advancewelcomebot.py source code. Removed Slash command support due to no permission handling. Now you need Message Content Intent enabled to use this source code. So much stuff has been changed since the last Discord.py 1.7.x commit.

--- README.txt
+++ README.txt
@@ -1,57 +1,24 @@
 WELCOME BOT WITH SLASH COMMANDS FOR DISCORD.PY
 
-This source code allows you to welcome members while having Slash commands work. Now has support where you can
-set up the bot for the Welcome module to work properly in multiple servers like what you see in bots like Dyno, Carl bot, etc
-Meaning you can set up a welcome channel per server and it will welcome members in each server seperatly just like popular bots like Dyno.
-Making this version more advanced then the last version.
-No need to make a bunch of discord bots and hosting them seperatly anymore for each server
-
+This source code allows you to welcome members while having Slash commands work.
 Includes:
-- Sending messages in a desired text channel (New version now supports having one bot in multiple servers and welcomes members in your desired set channel for each server.)
-- Gives an unverified role to a member with verification command
+- Sending messages in a desired text channel
+- Gives a role to a member
 - Sends a welcome message to a webhook
 - DMs members upon joining the server
-- Slash commands working with all of this.
+
+With the Discord.py 2.0 rewrite, Slash commands became normal commands because slash commands has a big
+problem where there is no permission handling in slash commands.
 
 ================================================
 
-REQUIREMENTS:
-- Knowledge of Discord.py
-- A discord bot application with token (with Server Members intent enabled)
-- Python 3.8.6 or newer (Using a Python software version below this version may have a chance to cause issues or errors)
+With the Discord.py 2.0 rewrite. The code has been improved a lot. Here is what has been changed since the last Discord.py 1.7.x release
+- Slash commands became normal commands (Default prefix is >)
+- Code specify you need Message Content Intent
+- Code suitable for public bots since we use json file to store data on where to send Logs to which channel and which server
+- Added Ping command.
 
 ================================================
-
-SETUP:
-1. Fork or Download this Source code to your hosting provider (or computer if you wish to self-host it)
-2. Create a Discord Bot Application and copy your bot token at https://discord.com/developers
-3. At the bottom, replace DISCORDBOTTOKEN with your Discord bot token you just created.
-4. Enable the Server Members intent in the developers portal. Should also unselect "Public Bot" in the developers portal as well.
-5. MODIFICATIONS:
-- At webhook = Webhook.from_url('WEBHOOKTOKEN', adapter=AsyncWebhookAdapter(session)), replace WEBHOOKTOKEN with the Webhook url (if you want the webhook)
-- At mutedRole = discord.utils.get(ctx.guild.roles, name="ROLENAMEHERE"), replace ROLENAMEHERE with the name of the role you want the bot to give in your server.
-6. Install discord.py and discord-py-slash-command
-7. Run the bot (I advise you test with an alt or with a friend to ensure you did everything correctly)
-
-You need to do nothing with the .json file. That file is already set to go.
-
-SET UP BOT ON DISCORD.
-1. Invite the bot to your server.
-2. Use /setwchannel command to change which text channel you want the welcome message to be sent to.
-3. Create an "Unverified" role and channel for new members to use the /verify command. If needed, run /verifysetup. To ensure no errors, give your bot admin.
-4. Repeat Steps 1-3 for each server you want your bot it.
-NOTE: The bot will not DM new members if you do not set up the role the bot gives unless you want to modify the code. However, you must know what you are doing.
-
-================================================
-
-NOTE: This source code was designed to be made for private utility bots in a few servers.
-You could have this a public bot but can not guarente proper working results.
-
-If your bot stays unverified and enabled the Message Content intent, you may be able to use regular commands
-instead.
-
-DISCLAIMER: THERE IS LITERALLY NO WARRENTY IF YOU DECIDE TO USE THIS SOURCE CODE. IF YOU DECIDE TO USE
-THIS SOURCE CODE, IT IS TO BE USED AT YOUR OWN RISK! WE WILL NOT BE HELD LIABLE FOR ANY DAMAGES WHAT SO EVER!
 
 READ THIS: I am unable to get a data reset command to work at all.
 The only way to remove your server/channel ids from the json file is too manually remove it from the file.
@@ -61,8 +28,35 @@ and modified it to a command but unable to get it to work. At this point, idk if
 I always get the following error: discord.ext.commands.errors.CommandInvokeError: Command raised an exception: KeyError: 'MYGUILDIDHERE'
 If you know how to fix this or create a data reset command, PLEASE let me know but submitting an issue or pull request.
 
-===============================================
+================================================
 
-CREDITS:
-https://stackoverflow.com/questions/64760333/how-to-set-a-welcome-channel-discord-py
-This help page is how I learned this.
+REQUIREMENTS:
+- Knowledge of Discord.py
+- A discord bot application with token
+- Python 3.8.6 or newer (Using a Python software version below this may cause issues)
+
+================================================
+
+SETUP:
+1. Fork or Download this Source code to your hosting provider
+2. Create a Discord Bot Application and copy your bot token at https://discord.com/developers
+3. At the bottom, replace DISCORDBOTTOKEN with your Discord bot token you just created.
+4. Enable the Server Members intent and the Message Content Intent in the developers portal. Should also unselect "Public Bot" in the developers portal as well.
+5. FOR EACH CLIENT EVENT (Developers mode needs to be on for this step):
+- At "guild = await client.fetch_guild(SERVERIDHERE)", replace SERVERIDHERE with the Server id your bot will be in
+- At webhook = Webhook.from_url('WEBHOOKTOKEN', adapter=AsyncWebhookAdapter(session)), replace WEBHOOKTOKEN with the Webhook url (if you want the webhook)
+- At mutedRole = discord.utils.get(ctx.guild.roles, name="ROLENAMEHERE"), replace ROLENAMEHERE with the name of the role you want the bot to give in your server.
+6. Install discord.py (Included in the requirements file)
+7. Run the bot
+8. Set up the Joins/Leaves log channel with the setwchannel command
+9. Run verifysetup if you need too
+10. You should be done (I advise you test with an alt or with a friend to ensure you did everything correctly)
+
+================================================
+
+DISCLAIMER: THERE IS LITERALLY NO WARRENTY IF YOU DECIDE TO USE THIS SOURCE CODE. IF YOU DECIDE TO USE
+THIS SOURCE CODE, IT IS TO BE USED AT YOUR OWN RISK! WE WILL NOT BE HELD LIABLE FOR DAMAGES!
+
+================================================
+
+CREDITS: https://stackoverflow.com/questions/64760333/how-to-set-a-welcome-channel-discord-py
--- main.py
+++ main.py
@@ -1,41 +1,42 @@
 import discord
 from discord.ext import commands
 import asyncio
-from discord import Webhook, AsyncWebhookAdapter
 import aiohttp
-from contextlib import redirect_stdout
-from discord_slash import SlashCommand
-import json
+import random
+import sys
 
 intents = discord.Intents.default()
+intents.message_content = True
 intents.members = True
 client = discord.Client(intents=intents)
-slash = SlashCommand(client, sync_commands=True)
+client = commands.Bot(command_prefix= commands.when_mentioned_or('>'), intents=intents)
+client.remove_command("help")
 
 @client.event
 async def on_ready():
     await client.change_presence(status=discord.Status.online)
-    print('logged in as **Bot**')
+    print('logged in as the Utility Bot.')
 
 @client.event
 async def on_member_join(member):
     with open('guilds.json', 'r', encoding='utf-8') as f:
         guilds_dict = json.load(f)
     channel_id = guilds_dict[str(member.guild.id)]
-    embed = discord.Embed(title=f"MEMBER JOIN | {member.guild.name}", description=f"Hello {member.mention} and Welcome to **{member.guild.name}**. Make sure to read the rules channel", color=(3140255))
+    embed = discord.Embed(title=f"MEMBER JOIN | {member.guild.name}", description=f"{member.name} has joined the server.", color=(65280))
     embed.set_thumbnail(url=member.avatar_url)
+    embed.set_footer(text="Open Source Code by LLoC Eagle Fan Art#1681")
     await client.get_channel(int(channel_id)).send(embed=embed)
-
-    async with aiohttp.ClientSession() as session:
-        webhook = Webhook.from_url('WEBHOOKTOKEN', adapter=AsyncWebhookAdapter(session))
-        await webhook.send(f'{member.mention} joined **{member.guild.name}**')
-
-    guild = member.guild
+    
+    guild = await client.fetch_guild(SERVERIDHERE)
     role = discord.utils.get(guild.roles, name='Unverified')
     await member.add_roles(role)
     
-    embed2 = discord.Embed(title="HI THERE", description=f"Welcome {member.mention} to **{member.guild.name}**", color=(3140255))
-    embed2.add_field(name="How to access the server?", value="In order to access the rest of the server, head over to the verify channel and type in **/verify** with me", inline=False)
+    async with aiohttp.ClientSession() as session:
+        webhook = discord.Webhook.from_url('WEBHOOKTOKEN', session=session)
+        await webhook.send(f'Hello {member.name} We are glad you are able to join us. Please make sure you read the rules and enjoy your stay here. In order to access the rest of the server, you need to verify yourself in the verify channel.')
+    
+    embed2 = discord.Embed(title="HI THERE", description=f"Welcome {member.name} to SERVER NAME HERE", color=(65280))
+    embed2.add_field(name=content="How to access the server?", value="In order to access the rest of the server, head to the verify channel and type in **>verify**", inline=False)
     await member.send(embed=embed2)
 
 @client.event
@@ -43,32 +44,31 @@ async def on_member_remove(member):
     with open('guilds.json', 'r', encoding='utf-8') as f:
         guilds_dict = json.load(f)
     channel_id = guilds_dict[str(member.guild.id)]
-    embed = discord.Embed(title=f"MEMBER LEAVE | {member.guild.name}", description=f"**{member.name}** has left **{member.guild.name}**. How sad", color=(16711680))
+    embed = discord.Embed(title=f"MEMBER LEAVE | {member.guild.name}", description=f"{member.name} has left the server. How sad", color=(16711680))
     await client.get_channel(int(channel_id)).send(embed=embed)
-    
-    async with aiohttp.ClientSession() as session:
-        webhook = Webhook.from_url('WEBHOOKTOKEN', adapter=AsyncWebhookAdapter(session))
-        await webhook.send(f'{member.name} left **CAT ROSALINA 18+**')
 
+@client.command()
+async def help(ctx):
+    embed = discord.Embed(title="advancedwelcomebot.py", description="Here are my commands", color=(49407))
+    embed.add_field(name="CORE COMMANDS", value=">help - This message\n>ping - Checks my latency", inline=False)
+    embed.add_field(name="UTILITY COMMANDS", value=">verify - Verify yourself into the server\n>setwchannel - Sets which channel to send Welcome logs to.\n>verifysetup - Sets up the Verification channel and role.", inline=False)
+    embed.add_field(name="SOURCE CODE", value="Original Source Code: https://github.com/ErisTheEagleArt/advancewelcomebot.py", inline=False)
+    await ctx.send(embed=embed)
 
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'My current latency is: {round(client.latency * 1000)}ms')
 
-
-
-@slash.slash(name="verify", description='Gain access to the server')
+@client.command()
 async def verify(ctx):
-    role = discord.utils.get(ctx.guild.roles, name="Unverified")
+    mutedRole = discord.utils.get(ctx.guild.roles, name="ROLENAMEHERE")
+    await ctx.author.remove_roles(mutedRole)
+    await ctx.send("You have been verified and have access to the rest of the server.")
+    embed = discord.Embed(title="SUCCESSFULLY VERIFIED", description=f"Thank you for verifying yourself into the server. Now you can access the rest of the server now.", color=(65535))
+    await ctx.author.send(embed=embed)
+    return
 
-    if role in ctx.author.roles:
-        await ctx.author.remove_roles(role)
-        await ctx.send("You have been verified and have access to the rest of the server.")
-        embed = discord.Embed(title="SUCCESSFULLY VERIFIED", description=f"Thank you for verifying yourself into the server. Now you can access the rest of the server now.", color=(3140255))
-        embed.set_footer(text=f"Successfully Verified in **{ctx.guild.name}")
-        await ctx.author.send(embed=embed)
-        return
-    else:
-        await ctx.send("You are already verified. No need to rerun this command.")
-
-@slash.slash(name="setwchannel", description='Sets which channel to send Welcome logs to for your current server.')
+@client.command()
 @commands.has_permissions(manage_channels=True)
 async def setwchannel(ctx, channel: discord.TextChannel):
     with open('guilds.json', 'r', encoding='utf-8') as f:
@@ -78,23 +78,9 @@ async def setwchannel(ctx, channel: discord.TextChannel):
     with open('guilds.json', 'w', encoding='utf-8') as f:
         json.dump(guilds_dict, f, indent=4, ensure_ascii=False)
     
-    await ctx.send(f'I set the welcome channel for **{ctx.guild.name}** to **{channel.name}**')
+    await ctx.send(f'I have set the welcome channel for **{ctx.message.guild.name}** to **{channel.name}**')
 
-@setwchannel.error
-async def setwchannel_error(ctx, error):
-    if isinstance(error, commands.MissingPermissions):
-        await ctx.send('HEY! YOU NEED TO HAVE THE **MANAGE_CHANNELS** PERMISSION TO CONTINUE')
-    else:
-        raise error
-
-@slash.slash(name="resources", description='See useful links and stats.')
-async def resources(ctx):
-    embed = discord.Embed(title="USEFUL RESOURCES", description="Here is some useful links and stats.", color=(3140255))
-    embed.add_field(name="Your Current Server Stats", value="Guild Name: {ctx.guild.name}\nGuild ID: {ctx.guild.id}\nGuild Member Count: {ctx.guild.member_count}", inline=False)
-    embed.add_field(name="ORIGNAL SOURCE", value="Orignal Source Code: https://github.com/ErisTheEagleArt/advancewelcomebot.py\nMade and Provided by: LLoC Eagle Fan Art#1681", inline=False)
-    await ctx.send(embed=embed)
-
-@slash.slash(name="verifysetup", description='Set up the Verification role and Verify channel.')
+@client.command()
 @commands.has_permissions(manage_roles=True, manage_channels=True)
 @commands.bot_has_permissions(manage_roles=True, manage_channels=True)
 async def verifysetup(ctx):
@@ -126,10 +112,18 @@ async def verifysetup(ctx):
     guild = ctx.guild
     channel = discord.utils.get(guild.text_channels, name="verify")
     role = discord.utils.get(guild.roles, name="Unverified")
-    await channel.send("WELCOME TO CAT ROSALINA 18+ | ERIS DEVELOPMENT\n\nTo prevent spam and abuse, be sure to verify yourself here by typing **/verify** with <@997300484132061184>\nWARNING: Even though there is no p*rn content here, however content in this server is suggestive which is intended for Mature Audiences\nPLEASE DO NOT JUDGE US WHEN YOU ACCESS THE SERVER!\n\nIf you are having issues verifying yourself, DM me and I will help you get verified.\nYou do NOT have to verify yourself, you can just chill here")
+    await channel.send("WELCOME TO YOURSERVERNAMEHERE\n\nTo prevent spam and abuse, be sure to verify yourself here by typing **/verify** with <@YOURBOTIDHERE>")
     await channel.set_permissions(role, send_messages=True, read_messages=True)
     await ctx.send("The **Unverified** role the the **verify** text channel has been set up again")
 
+@verifysetup.error
+async def verifysetup_error(ctx, error):
+    if isinstance(error, commands.BotMissingPermissions):
+        await ctx.send("HEY! YOU NEED TO HAVE THE **MANAGE_CHANNELS** AND **MANAGE_ROLE** PERMISSION TO CONTINUE")
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send('SYSTEM ERROR: I require the **MANAGE_CHANNELS** and **MANAGE_ROLES** Permission. Recommended to give me Admin to prevent errors.')
+    else:
+        raise error
 
 
 
